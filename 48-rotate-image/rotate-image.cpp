55605962b6645054cdd7c64d0a19860e06d0771e
Ritika Soni <ritikasoni6759@gmail.com>
2026-07-25 01:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (93.94%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<m;j++)
+            {
+               swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        for(int i=0;i<m;i++)
+        {
+            int l=0,r=m-1;
+            while(l<=r)
+            {
+                swap(matrix[i][l],matrix[i][r]);
+                l++;
+                r--;
+            }
+        }
+    }
+};
