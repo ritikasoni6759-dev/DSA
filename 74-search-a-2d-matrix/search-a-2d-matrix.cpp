887f53178943b1d3407e1e4a73d3748d371142e0
Ritika Soni <ritikasoni6759@gmail.com>
2026-07-25 00:46:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (98.87%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        int l=0;
+        int r=m*n-1;
+
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+
+            int row=mid/m;
+            int col=mid%m;
+
+            if(matrix[row][col]==target)
+            return true;
+
+            else if(matrix[row][col]>target)
+            r=mid-1;
+
+            else 
+            l=mid+1;
+        }
+
+        return false;
+    }
+};
