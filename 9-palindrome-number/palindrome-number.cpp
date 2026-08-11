fc3fd95ba8d7a47a82cf92b9ee39bce8a3bd55ad
Ritika Soni <ritikasoni6759@gmail.com>
2026-08-12 00:29:11 +0530

    Time: 3 ms (44.15%) | Memory: 8.6 MB (65.42%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long n=x;
-        long long res=0;
+        int n=x;
+        long long int res=0;
         while(n>0)
         {
             long long ld=n%10;
              res=res*10+ld;
             n=n/10;
         }
-        if(res==x)
-        return true;
-        else return false;
+        
+        return res==x;
+     
     }
 };
