b321e55447a9cf3cac9fd762cfacaa36d784ebb9
Ritika Soni <ritikasoni6759@gmail.com>
2026-08-14 02:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (94.54%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count=0;
-        int n=nums.size();
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-            
-        }
-        for(int i=0;i<n;i++)
-        {
-          if(mp[nums[i]]>n/2)
-          count=nums[i];
-        }
+        //Boyer Moore Voting Algorithm
 
-        return count;
+        int candidate=0;
+        int count=0;
+
+        for(int num:nums)
+        {
+            if(count==0)
+            candidate=num;
+
+            if(num==candidate)
+            count++;
+            else
+            count--;
+        }
+        return candidate;
     }
 };
