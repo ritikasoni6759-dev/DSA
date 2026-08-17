42fdce711147d3cc67e872f5f83de02836c4b8ef
Ritika Soni <ritikasoni6759@gmail.com>
2026-08-18 01:02:37 +0530

    Time: 40 ms (68.79%) | Memory: 84.8 MB (63.21%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l=0;int ans=0;
+        unordered_map<int,int> mp;
+        for(int r=0;r<fruits.size();r++){
+            mp[fruits[r]]++;
+
+            while(mp.size()>2)
+            {
+                mp[fruits[l]]--;
+                if(mp[fruits[l]]==0)
+                mp.erase(fruits[l]);
+                l++;
+            }
+            ans=max(ans,r-l+1);
+
+        }
+        return ans;
+    }
+};
